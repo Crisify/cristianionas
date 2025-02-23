adfe7bf31117c15e96d50ab33961f1a798f29fe9
Cristian Ionas <cristian@crisify.com>
2025-02-23 23:07:03 +0200

    update latest artifacts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'crisionas.github.io'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('crisionas.github.io');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crisionas.github.io app is running!');
-  });
 });
