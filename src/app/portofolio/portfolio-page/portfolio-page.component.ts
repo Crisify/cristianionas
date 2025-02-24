744b15ed4bbece3d7cf8004f215077b36a3e728e
Cristian Ionas <cristian@crisify.com>
2025-02-24 22:17:29 +0200

    update project changes

--- src/app/portofolio/portfolio-page/portfolio-page.component.ts
+++ src/app/portofolio/portfolio-page/portfolio-page.component.ts
@@ -9,9 +9,9 @@ export class PortfolioPageComponent {
 
   projects = [
     {title: 'Insurance Processes', image: 'assets/images/projects/insurance/overview.png', projectNameRoute: "insurance-processes"},
-    {title: 'Broker Cloud', image: 'assets/images/projects/brokerCloud/overview.png', projectNameRoute: "broker-cloud"},
-    {title: 'Notification Service', image: 'assets/images/projects/notificationService/overview.png', projectNameRoute: "notification-service"},
     {title: 'One Directory Sync', image: 'assets/images/projects/ods/overview.png', projectNameRoute: "one-directory-sync"},
+    {title: 'Notification Service', image: 'assets/images/projects/notificationService/overview.png', projectNameRoute: "notification-service"},
+    {title: 'Broker Cloud', image: 'assets/images/projects/brokerCloud/overview.png', projectNameRoute: "broker-cloud"},
     {title: 'Directory Sync', image: 'assets/images/projects/directorySync/directory.png', projectNameRoute: "directory-sync"},
     {title: 'Migration Tool', image: 'assets/images/projects/migrationTool/overview.png', projectNameRoute: "migration-tool"},
     {title: 'SecureVote', image: 'assets/images/projects/votingSystem/blockchain-voting.png', projectNameRoute: "voting-system"}
